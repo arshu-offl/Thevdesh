d0b6b1eca7ef8a0b8c3dd2faf4012d3ef2d60a20
Arshad <arshuoffl@gmail.com>
2025-11-22 00:52:04 +0530

    Poo Poo for Q4

--- Arshad.cpp
+++ Arshad.cpp
@@ -1,1 +1,32 @@
 
+// Poo Poo O(q * q)
+vector<int> solution(vector<int> queries){
+    
+    unordered_map<int, int> hashMap;
+    int mx = 0;
+    vector<int> ans;
+    for(int q: queries){
+       
+        hashMap[q] = 1;
+        
+        int curr = 1;
+        int temp = q;
+        
+        // Find contigous
+        while(hashMap.count(temp - 1)){
+            curr++;
+            temp--;
+        }
+        
+        temp = q;
+        while(hashMap.count(temp + 1)){
+            curr++;
+            temp++;
+        }
+        
+        mx = max(mx, curr);
+        ans.push_back(mx);
+    }
+    
+    return ans;
+}
